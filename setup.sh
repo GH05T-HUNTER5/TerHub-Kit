8b164e024fd03ad875450ec246534b6930506da0
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-07-07 22:29:41 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,62 @@
+##################################################
+#                 GH05T-HUNTER5                  #
+##################################################
+# Very advanced hacking tools are available here #
+#  Phishing  ,  Brute Force  , Penetration test  #
+##################################################
+clear
+bans () {
+echo ""
+echo -e " \033[0;32m ████████╗███████╗██████╗ ██╗  ██╗██╗   ██╗██████╗     ██╗  ██╗██╗████████╗"
+echo -e " \033[0;32m ╚══██╔══╝██╔════╝██╔══██╗██║  ██║██║   ██║██╔══██╗    ██║ ██╔╝██║╚══██╔══╝"
+echo -e " \033[0;32m    ██║   █████╗  ██████╔╝███████║██║   ██║██████╔╝    █████╔╝ ██║   ██║"
+echo -e " \033[0;32m    ██║   ██╔══╝  ██╔══██╗██╔══██║██║   ██║██╔══██╗    ██╔═██╗ ██║   ██║"
+echo -e " \033[0;32m    ██║   ███████╗██║  ██║██║  ██║╚██████╔╝██████╔╝    ██║  ██╗██║   ██║"
+echo -e " \033[0;32m    ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═════╝     ╚═╝  ╚═╝╚═╝   ╚═╝"
+}
+bans
+echo ""
+echo -e "          \033[1;37m ##################################################"
+echo -e "          \033[1;37m #             \033[0;32m    GH05T-HUNTER5              \033[1;37m    #"
+echo -e "          \033[1;37m ##################################################"
+echo -e "          \033[1;37m #\033[0;34m Very advanced hacking tools are available here\033[1;37m #"
+echo -e "          \033[1;37m # \033[0;34m Phishing  ,  Brute Force  , Penetration test\033[1;37m  #"    
+echo -e "          \033[1;37m #================================================#"
+echo -e "          \033[1;37m #           \033[0;31m        DISCLAIMER                 \033[1;37m  #"    
+echo -e "          \033[1;37m #                  ============                \033[1;37m  #"
+echo -e "          \033[1;37m #\033[0;32m We will not be responsible if anything happens\033[1;37m #"
+echo -e "          \033[1;37m #   \033[0;32m        to you using the tool here         \033[1;37m  #"
+echo -e "          \033[1;37m ##################################################"
+sleep 5
+echo ""
+read -p "  I take full responsibility [ yes , no ] : " yes
+if [[ $yes == "yes" ]] || [[ $yes == "Yes" ]] || [[ $yes == "YES" ]] || [[ $yes == "y" ]] || [[ $yes == "Y" ]]; then
+echo " Completed"
+clear
+elif [[ $yes == "No " ]] || [[ $yes == "no" ]] || [[ $yes == "NO" ]] || [[ $yes == "N"  ]] || [[ $yes == "n" ]]; then
+echo ""
+cd ..
+rm -rf TerHub-Kit
+else
+echo ""
+clear
+bash setup.sh
+fi
+main () {
+pkg install python -y >> /dev/null 2>&1
+pkg install python2 -y >> /dev/null 2>&1
+apt install php -y >> /dev/null 2>&1
+apt install ruby -y >> /dev/null 2>&1
+apt-get install ruby -y >> /dev/null 2>&1
+apt install gem -y >> /dev/null 2>&1
+apt install curl -y >> /dev/null 2>&1
+pkg install wget -y >> /dev/null 2>&1
+}
+clear
+bans
+echo -e "\033[1;37m ###########################################################################"
+echo -e "\033[1;37m #          \033[0;32m      ######   Installation Successfull   #####        \033[1;37m        #"
+echo -e "\033[1;37m ###########################################################################"
+sleep 3
+clear
+exit
